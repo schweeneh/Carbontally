186adfec11b21ed251e9590fcc45daa997628add
schweeneh <chrispsweeney@gmail.com>
2014-02-02 14:12:47 -0600

    Removed database password and username.

--- public_html/include/dbconfig.php
+++ public_html/include/dbconfig.php
@@ -1,10 +1,6 @@
 <?php
 $servername = 'localhost';
-$username = 'Footprint';
-$password = 'C4rb0nt4lly';
-$database = 'carbontally';
-/*$servername = 'db419539768.db.1and1.com';
-$username = 'dbo419539768';
-$password = '!Walkers1';
-$database = 'db419539768';*/
+$username = '';
+$password = '';
+$database = '';
 ?>
